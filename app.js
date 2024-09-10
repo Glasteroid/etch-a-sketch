05909556cfd3e137a0ed45c08d3b95faeefa99ec
Glasteroid <kennethangeles880@yahoo.com>
2024-09-09 22:40:08 -0700

    modified everything lol

---
+++ app.js
@@ -0,0 +1,32 @@
+const wrapper = document.querySelector(".wrapper");
+const button = document.querySelector("button");
+
+function createRows(size) {
+    const squareSize = 400 / size;
+
+    wrapper.style.width = '400px';
+    wrapper.style.height = '400px';
+    
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.className = "square";
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        wrapper.appendChild(div);
+    }
+
+    const boxes = document.querySelectorAll(".square");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", (event) => {
+            event.target.style.background = "black";
+        });
+    });
+}
+
+createRows(16);
+
+button.addEventListener("click", () => {
+    const size = prompt("How big would you like x to be, where the grid is made up of x by x squares?");
+    wrapper.innerHTML = '';
+    createRows(size);
+});
